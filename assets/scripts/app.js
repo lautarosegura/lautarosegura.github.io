d917931b672f8ff0163012bce322ff05297f4ed1
lautarosegura <lautarooseguraa@gmail.com>
2023-04-26 15:55:30 -0300

    fix

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,87 +1,80 @@
 //AIzaSyCcQe
 
-
 //M1pOjId32zq
 
 //L2zWiPXIwTbAPg1nLsasdasd
 
-$(document).ready(function() {
-    $('#fare-calculator').submit(function(event) {
-        event.preventDefault();
-        const distance = parseFloat($('#distance').val());
-        const nightTime = $('#night-time').prop('checked');
-        const withReturn = $('#return').prop('checked');
-        const fare = calculateFare(distance, nightTime, withReturn);
-        $('#result').text('Coste estimado: $' + fare.toFixed(2));
-    })
+$(document).ready(function () {
+	$('#fare-calculator').submit(function (event) {
+		event.preventDefault();
+		const distance = parseFloat($('#distance').val());
+		const nightTime = $('#night-time').prop('checked');
+		const withReturn = $('#return').prop('checked');
+		const fare = calculateFare(distance, nightTime, withReturn);
+		$('#result').text('Coste estimado: $' + fare.toFixed(2));
+	});
 
-    $('.shortcut').click(function(event) {
-        event.preventDefault();
-        $('.list-group-item').removeClass('active');
-        $(this).parent().addClass('active');
-        var target = event.target.dataset.target;
-        let distance;
-        switch(target) {
-            case 'lp-ezeiza':
-                distance = 84.0;
-            break;
-            case 'ezeiza-lp':
-                distance = 84.9;
-            break;
-            case 'lp-aeroparque':
-                distance = 67.1;
-            break;
-            case 'aeroparque-lp':
-                distance = 65.5;
-            break;
-        }
+	$('.shortcut').click(function (event) {
+		event.preventDefault();
+		$('.list-group-item').removeClass('active');
+		$(this).parent().addClass('active');
+		var target = event.target.dataset.target;
+		let distance;
+		switch (target) {
+			case 'lp-ezeiza':
+				distance = 84.0;
+				break;
+			case 'ezeiza-lp':
+				distance = 84.9;
+				break;
+			case 'lp-aeroparque':
+				distance = 67.1;
+				break;
+			case 'aeroparque-lp':
+				distance = 65.5;
+				break;
+		}
 
-        $('#distance').val(distance);
-        $('#night-time, #return').prop('checked', false)
-    });
+		$('#distance').val(distance);
+		$('#night-time, #return').prop('checked', false);
+	});
 
-    $(document).on('input', '#distance', function() {
-        $('.list-group-item').removeClass('active');
-        $('#night-time, #return').prop('checked', false)
-    })
+	$(document).on('input', '#distance', function () {
+		$('.list-group-item').removeClass('active');
+		$('#night-time, #return').prop('checked', false);
+	});
 
-    $(document).on('input', function() {
-        $('#result').text('');
-    })
+	$(document).on('input', function () {
+		$('#result').text('');
+	});
 
-    $('#clear').click(function(event) {
-        event.preventDefault();
-        $('#distance').val('');
-        $('#night-time, #return').prop('checked', false);
-        $('.list-group-item').removeClass('active');
-    })
-
+	$('#clear').click(function (event) {
+		event.preventDefault();
+		$('#distance').val('');
+		$('#night-time, #return').prop('checked', false);
+		$('.list-group-item').removeClass('active');
+	});
 });
 
-
 function calculateFare(distance, nightTime, withReturn) {
-    const baseFare = 200;
-    const pricePerKilometer = distance > 10 ? 170 : 180;
-    const nightTimeSurcharge = 0.15;
-    const extraSurcharge = 0.06;
-    const shortReturnSurcharge = 1;
-    const longReturnSurcharge = 0.3;
+	const baseFare = 200;
+	const pricePerKilometer = distance > 10 ? 170 : 180;
+	const nightTimeSurcharge = 0.15;
+	const shortReturnSurcharge = 1;
+	const longReturnSurcharge = 0.3;
 
-    let fare = baseFare + distance * pricePerKilometer;
-    if (withReturn) {
-        if (distance <= 30 ) {
-            fare += fare * shortReturnSurcharge;
-        } else if (distance > 30 && distance < 100) {
-            fare += fare * longReturnSurcharge;
-        }
-    }
+	let fare = baseFare + distance * pricePerKilometer;
+	if (withReturn) {
+		if (distance <= 30) {
+			fare += fare * shortReturnSurcharge;
+		} else if (distance > 30 && distance < 100) {
+			fare += fare * longReturnSurcharge;
+		}
+	}
 
-    if (nightTime) {
-        fare += fare * nightTimeSurcharge;
-    }
+	if (nightTime) {
+		fare += fare * nightTimeSurcharge;
+	}
 
-    fare += fare * extraSurcharge;
-
-    return fare;
-
+	return fare;
 }
